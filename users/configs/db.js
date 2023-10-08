bfe407208d2d1d7f574075fcd9246558a7ecb475
trinanjan <trinanjan12@gmail.com>
2023-10-08 17:07:10 +0530

    trinanjan:adding changes for docker integration

--- users/configs/db.js
+++ users/configs/db.js
@@ -5,12 +5,13 @@ require('dotenv').config();
 const { DB_HOST,
     DB_USER,
     DB_PASSWORD,
-    DB_NAME } = process.env;
+    DB_NAME,DB_PORT} = process.env;
 
 const db = mysql.createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
+    port:DB_PORT,
     database: DB_NAME,
     namedPlaceholders: true,
 });
